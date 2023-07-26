d68fdad2e75b7353107754d5b73cb72d659b3a8a
Raza <42661870+AlmostEfficient@users.noreply.github.com>
2023-07-27 06:14:01 +1200

    Commit message

---
+++ docs/intro.md
@@ -0,0 +1,97 @@
+---
+sidebar_position: 1
+slug: /
+# hide_table_of_contents: true
+description: "A collection of resources for the OPOS Hackathon."
+---
+
+# What is Only Possible on Solana? (WIP)
+
+{video here explaining what/why of OPOS}
+
+<!-- {%youtube ytvidIdhere %} -->
+
+Paths:
+- Solana Mobile
+- DePIN
+- Compression
+- Payments
+- DeFi 
+- xNFTs
+- Onboarding
+
+Examples:  
+[Compression - Drip Haus](http://drip.haus/) - Free art dropped to wallets daily/weekly.  
+[Durable nonces - Code Wallet](https://www.getcode.com/) - Instant transactions  
+[Openbook - Cypher/Dift](https://cypher.trade/) - On-chain central limit orderbook
+
+# Get started
+Set up a dApp in one command with [Create Solana Dapp](https://github.com/solana-developers/create-solana-dapp)
+```bash
+npx create-solana-dapp
+```
+<!-- # Forkable repos -->
+
+
+# Templates, guides, and docs
+## OPOS specials
+### Durable Nonces: Instant & offline transactions
+[Offline Transaction Signing using Durable Nonces](https://solana.com/developers/guides/advanced/introduction-to-durable-nonces)
+
+[Deep dive on durable nonces](https://www.youtube.com/watch?v=1A-sG1xa_zg)
+
+[Instant transactions with the Timelock program (built by Code Wallet)](https://github.com/code-wallet/code-program-library/blob/main/timelock/README.md)
+
+[Offline transaction signing demo repo](https://github.com/0xproflupin/solana-durable-nonces/tree/main) - + Accompanying video guide [here](https://www.youtube.com/watch?v=2K8qU__Uo3s)
+
+### Compression: Cheap NFTs
+[What is compression?](https://docs.helius.xyz/solana-compression-and-das-api/what-is-compression)
+
+[Compression calculator](https://compressed.app/)
+
+[Creating compressed NFTs with Javascript](https://solana.com/developers/guides/javascript/compressed-nfts)
+
+### Solang: write Solana programs in Solidity
+[Getting started with Solang](https://solana.com/developers/guides/solang/getting-started)
+
+### xNFTs: Executable NFTs
+[xNFT Docs](https://docs.xnft.gg/)
+
+[xNFT quickstart - build an xNFT in minutes](https://github.com/coral-xyz/xnft-quickstart)
+
+### Solana Pay: the smoothest payments spec in crypto
+[Create transaction QR codes with Solana Pay](https://blog.avneesh.tech/what-is-solana-pay-and-how-to-use-it-in-your-nextjs-app)
+
+[Take payments with QR codes using Solana Pay](https://www.quicknode.com/guides/solana-development/solana-pay/add-solana-pay-to-your-web-app)
+
+### Solana Mobile Stack: Mobile native web3
+[Solana Mobile Docs](https://docs.solanamobile.com/getting-started/intro)
+
+[Solana Mobile dApp Scaffold](https://github.com/solana-mobile/solana-mobile-dapp-scaffold)
+
+[MintyFresh - mint NFTs from images on your phone](https://github.com/solana-mobile/Minty-fresh)
+
+### Openbook: The thinnest spreads on earth, all on-chain
+Missing quickstarts/ez setups
+
+https://github.com/openbook-dex/resources
+
+## General resources
+[Web3.js examples - everything you need to make your app do things using on chain programs](https://github.com/solana-developers/web3-examples)
+
+[Program examples](https://github.com/solana-developers/program-examples)
+
+[Massive list of most Solana Programming Resources](https://github.com/SolanaNatives/Solana-Programming-Resources)
+
+[Official Solana Docs](https://docs.solana.com/getstarted/hello-world)
+
+[How to start a startup by Paul Graham](http://www.paulgraham.com/start.html)
+
+[Stuck? Need help? Ask a question here and then take a nap](https://solana.stackexchange.com/)
+
+### Solana Figma kits
+[Solana Pay Hackathon Kit](https://www.figma.com/community/file/1070574785723157359/Solana-Pay-Hackathon-Kits)
+
+[SMS Hacker House kit](https://www.figma.com/community/file/1144333121848437366/SMS-Hacker-House-Kit)
+
+[Solana Pay buttons](https://www.figma.com/community/file/1070341985720702755/Solana-Pay-Buttons)
---
+++ docusaurus.config.js
@@ -0,0 +1,136 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Only Possible On Solana',
+  tagline: 'Explore things that are only possible on Solana',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://opos.wtf',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  organizationName: 'almostefficient',
+  projectName: 'opos', 
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: false,
+          // Remove this to remove the "edit this page" links.
+          editUrl: 'https://github.com/AlmostEfficient/opos',
+          routeBasePath: '/',
+        },
+        // blog: {
+        //   showReadingTime: true,
+        //   // Please change this to your repo.
+        //   // Remove this to remove the "edit this page" links.
+        //   editUrl:
+        //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        // },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      // image: 'img/docusaurus-social-card.jpg',
+      defaultMode: 'dark',
+      navbar: {
+        title: 'Only Possible on Solana',
+        logo: {
+          alt: 'OPOS Logo',
+          src: 'img/emoji-thinking.gif',
+        },
+        items: [
+          // {
+          //   type: 'docSidebar',
+          //   sidebarId: 'tutorialSidebar',
+          //   position: 'left',
+          //   label: 'Tutorial',
+          // },
+          // {to: '/blog', label: 'Blog', position: 'left'},
+          // {
+          //   href: 'https://github.com/facebook/docusaurus',
+          //   label: 'GitHub',
+          //   position: 'right',
+          // },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Solana Developer Portal',
+                href: 'http://solana.com/developers',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Exchange',
+                href: 'https://solana.stackexchange.com/',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discord.gg/8kqgQTYbGy',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/lamportDAO',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'SolDev Solana Course',
+                href: 'https://www.soldev.app/course',
+              },
+              {
+                label: 'Solana Cookbook',
+                href: 'https://solanacookbook.com/',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Glass Chewers, Inc.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
